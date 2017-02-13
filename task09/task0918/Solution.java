c73d95c9d2d87929a703353c676b7b083aff2f99
MariaSw <kryvunmaria@gmail.com>
2017-02-13 15:05:38 +0400

    на создание исключений путем наследования

---
+++ task09/task0918/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task09.task0918;
+
+/* 
+Все свои, даже исключения
+*/
+
+
+import java.io.FileNotFoundException;
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+    
+    static class MyException extends FileNotFoundException {
+    }
+
+    static class MyException2 extends NoSuchFieldException{
+    }
+
+    static class MyException3 extends IndexOutOfBoundsException{
+    }
+
+    static class MyException4 extends NullPointerException{
+    }
+}
+
