c30fcd8b694f06fb29795fe3ccb7382547e3ca0e
MariaSw <kryvunmaria@gmail.com>
2017-02-13 22:12:51 +0400

    Бобров, деление на ноль

---
+++ task09/task0919/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task09.task0919;
+
+/* 
+Деление на ноль
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try {
+            divisionByZero();
+        }
+        catch (ArithmeticException e){
+            e.printStackTrace();
+        }
+    }
+    public static void divisionByZero() throws ArithmeticException {
+        int a =16/0;
+        System.out.println(a);
+    }
+}
