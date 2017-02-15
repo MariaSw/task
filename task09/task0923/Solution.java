bc8f5672d4f15b7f1cf0a0d6ddc530324f93d612
MariaSw <kryvunmaria@gmail.com>
2017-02-15 18:23:51 +0400

    Бобров, на вывод гласных, согласных из строки

---
+++ task09/task0923/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task09.task0923;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Гласные и согласные
+*/
+
+public class Solution {
+    public static char[] vowels = new char[]{'а', 'я', 'у', 'ю', 'и', 'ы', 'э', 'е', 'о', 'ё'};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String text = reader.readLine();
+
+        char[] textChars = text.toCharArray();
+        String vowSt="";
+        String consSt="";
+
+        for (char a:textChars) {
+            if (isVowel(a)) vowSt += a + " ";
+            else if (a != ' ') consSt += a + " ";
+        }
+        System.out.println(vowSt);
+        System.out.println(consSt);
+
+        //напишите тут ваш код
+    }
+
+    // метод проверяет, гласная ли буква
+    public static boolean isVowel(char c) {
+        c = Character.toLowerCase(c);  // приводим символ в нижний регистр - от заглавных к строчным буквам
+
+        for (char d : vowels)   // ищем среди массива гласных
+        {
+            if (c == d)
+                return true;
+        }
+        return false;
+    }
+}
